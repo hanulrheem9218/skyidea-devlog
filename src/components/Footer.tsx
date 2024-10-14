c46028311e1d3052c7471b37eef696a79e90e377
rheemhanul9218 <rheemhanul@gmail.com>
2024-10-14 22:11:06 +1300

    feat: add audio player

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import { FaSass } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { SiVite } from "react-icons/si";
 const Footer = () => {
   return (
     <div className="footer">
-        <div>Logo</div>
+        <div>Developed with ...<SiVite color="white" /><SiTypescript color="white"/><FaSass color="white" /><FaReact color="white"/></div>
         <div>"Skyidea's personal devlog"</div>
         <div>?</div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
+import { GiSpeaker, GiSpeakerOff } from "react-icons/gi";
 const Navbar = () => {
+  const [speaker,setSpeaker] = useState(false);
+  const audio = document.getElementById("audio-player") as HTMLAudioElement;
+  const toggle = () => {
+    if(speaker && audio ) audio.play();
+    else audio.pause();
+    setSpeaker(!speaker)
+  };
   return (
     <div className="navbar">
-        <img className="navbar -img"src= "/skyidea-white.svg"/>
-     
+      <audio id="audio-player" >
+        <source src="./ambient.mp3" type="audio/mpeg"/>
+      </audio>
+      <img className="navbar -img" src="/skyidea-white.svg" />
       <ul className="navbar text -small">
-        <li ><a className="navbar -list" href="/">Personal</a></li>
-        <li ><a className="navbar -list" href="/">Devlog</a></li>
+        <li>
+           {speaker ?  <GiSpeakerOff onClick={toggle}/>: <GiSpeaker onClick={toggle}/>} 
+        </li>
+        <li>
+          <a className="navbar -list" href="/">
+            Personal
+          </a>
+        </li>
+        <li>
+          <a className="navbar -list" href="/">
+            Devlog
+          </a>
+        </li>
       </ul>
     </div>
   );
