1b0eeee2a3f68d1b0d6c4488b7d2ca2d7cca18c5
rheemhanul9218 <rheemhanul@gmail.com>
2024-10-15 19:24:26 +1300

    feat: update contents

--- src/pages/Blank.tsx
+++ src/pages/Blank.tsx
@@ -8,12 +8,12 @@ import { useNavigate } from "react-router-dom";
 const Blank = () => {
   const navigator = useNavigate();
   const datas: CardProps[] = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 1; i++) {
     datas.push({
       index: i,
       title: `My Junior Artist experience.. ${i}`,
       description: "what's game developer ? and who is it for",
-      imgSrc: "../exampleDraw.jpg",
+      imgSrc: "../test04.png",
       date: new Date("2024-10-13"),
       tags: ["About Life", "Beginning Chapter", "Personal"],
     });
