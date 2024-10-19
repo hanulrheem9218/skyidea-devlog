5e335a72eb9ccf39d246e4034eb8f9d2d7abe95e
rheemhanul9218 <rheemhanul@gmail.com>
2024-10-19 22:34:22 +1300

    feat: add video title

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,10 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { IoMdDocument } from "react-icons/io";
 const VideoPlayer = () => {
+    const navigator = useNavigate();
   return (
     <div className="video-container">
       <video className="video" autoPlay muted loop playsInline>
         <source src="intro-skyidea.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <div className="video-description">
+        <h3 className="video-title">Welcome.</h3>
+        <p className="video-paragraph">Skyidea's Official Game Devlog Website.</p>
+        <button onClick={() => {navigator("/devlog")}} className="video-button"><IoMdDocument  className="video-icon"/>Devlog</button>
+      </div>
+      
     </div>
   );
 };
